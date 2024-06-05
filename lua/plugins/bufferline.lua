d9360b3c61a39277cbabfab18a2ef073ef33f89d
YSNBX <yasinnbeyhann@gmail.com>
2024-06-05 02:05:15 +0200

    modified bufferline wrong property placement

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,10 +1,6 @@
 return {
 	"akinsho/bufferline.nvim",
 	event = { "BufReadPost", "BufNewFile"},
-	keys = {
-		{ "<A-h>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Previous Buffer" },
-		{ "<A-l>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next Buffer" },
-	},
 	config = function()
 		require("bufferline").setup{
 			options = {
@@ -26,8 +22,12 @@ return {
 					enabled = true,
 					delay = 0,
 					reveal = { "close" }
-				}
-			}
+				},
+				keys = {
+					{ "<A-h>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Previous Buffer" },
+					{ "<A-l>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next Buffer" },
+				},
+			},
 		}
 	end
 }
