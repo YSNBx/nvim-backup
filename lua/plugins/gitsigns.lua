20653576cc963444d791283100d31db64c8bef61
Ysn <yasinnbeyhann@gmail.com>
2025-02-02 01:03:45 +0100

    modified gitsigns
    changed next/previous hunk keymappings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,14 +3,18 @@ return {
 	event = "VeryLazy",
 	config = true,
 	opts = {
+		current_line_blame_opts = {
+			delay = 500
+		},
 		on_attach = function(bufnr)
 			local gs = package.loaded.gitsigns
 
 			local function map(mode, l, r, desc)
 				vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
 			end
-			map("n", "]h", gs.next_hunk, "Next Hunk")
-			map("n", "[h", gs.prev_hunk, "Previous Hunk")
+
+			map("n", "hn", gs.next_hunk, "Next Hunk")
+			map("n", "hp", gs.prev_hunk, "Previous Hunk")
 
 			map("n", "<leader>hs", gs.stage_hunk, "Stage Hunk")
 			map("n", "<leader>hr", gs.reset_hunk, "Reset Hunk")
